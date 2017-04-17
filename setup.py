d5369a78ca1fa1cf09adba652d9bd6f9565a7d32
Leonardo Arroyo <contato@leonardoarroyo.com>
2017-04-17 19:39:17 -0300

    Update requirements

--- setup.py
+++ setup.py
@@ -1,5 +1,9 @@
 # -*- coding: utf-8 -*-
 from setuptools import setup, find_packages
+from pip.req import parse_requirements
+
+install_reqs = parse_requirements(<requirements_path>)
+reqs = [str(ir.req) for ir in install_reqs]
 
 setup(
     name='ovp-core',
@@ -16,11 +20,5 @@ setup(
                 ' and retrieving.',
     long_description=open('README.rst', encoding='utf-8').read(),
     zip_safe=False,
-    install_requires = [
-      'Django>=1.10.1,<1.11.0',
-      'djangorestframework>=3.5.3,<3.6.0',
-      'codecov>=2.0.5,<2.1.0',
-      'coverage>=4.2,<4.3.0',
-      'django-vinaigrette>=1.0.1,<1.1.0',
-    ]
+    install_requires = reqs
 )
