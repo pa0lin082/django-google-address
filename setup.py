b2c037c8c0bc9f4a7b920e4eddee7b538d0fe8c6
Paolo Leggio <paolo@digitalmonkeys.it>
2020-04-30 01:02:38 +0200

    fix setup

--- setup.py
+++ setup.py
@@ -7,7 +7,11 @@ except ImportError: # for pip <= 9.0.3
     from pip.req import parse_requirements
 
 install_reqs = parse_requirements('requirements.txt', session=False)
-reqs = [str(ir.req) for ir in install_reqs]
+try:
+    reqs = [str(ir.req) for ir in install_reqs]
+except AttributeError:
+    reqs = [str(ir.requirement) for ir in install_reqs]
+
 
 setup(
     name='django-google-address',
