4c141e03a5b19df9ad76a35f4da939624251e448
Paolo Leggio <paolo@digitalmonkeys.it>
2020-05-06 16:13:50 +0200

    test

--- setup.py
+++ setup.py
@@ -7,6 +7,7 @@ except ImportError: # for pip <= 9.0.3
     from pip.req import parse_requirements
 
 install_reqs = parse_requirements('requirements.txt', session=False)
+
 try:
     reqs = [str(ir.req) for ir in install_reqs]
 except AttributeError:
